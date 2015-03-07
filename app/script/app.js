8394583bd1f9618e81784b4dde784877bd95ba93
Xaosaki <xaosaki@gmail.com>
2015-03-08 00:23:54 +0300

    Освоился с AngularJS, набросал маршрутизацию.

--- app/script/app.js
+++ app/script/app.js
@@ -1,3 +1,43 @@
-var m = "String";
+var pbApp = angular.module('pbApp', ['ngRoute']);
 
-console.log(m);
+pbApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/',{
+			templateUrl:'all.html',
+			controller:'ListCtrl'
+		})
+		.when('/new',{
+			templateUrl:'contact.html',
+			controller:'newCtrl'
+		})
+		.when('/user/:userId',{
+			templateUrl:'user.html',
+			controller:'userInfoCtrl'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
+
+pbApp.controller('ListCtrl',['$scope','$http','$location', function ($scope, $http, $location) {
+	$scope.title = "Контакты"
+	$http.get('../../contacts.json').success(function(data,status,headers,config){
+		$scope.contacts = data;
+	});
+}]);
+
+pbApp.controller('newCtrl',['$scope','$http','$location', function ($scope, $http, $location) {
+	$scope.title = "Новый контакт"
+
+	$http.get('../../contacts.json').success(function(data,status,headers,config){
+		$scope.contacts = data;
+	});
+}]);
+
+pbApp.controller('userInfoCtrl',['$scope','$http','$location','$routeParams', function ($scope, $http, $location, $routeParams) {
+	$scope.title = "Изменить контакт";
+	$scope.userId = $routeParams.userId;
+	$http.get('../../contacts.json').success(function(data,status,headers,config){
+		$scope.contacts = data;
+	});
+}]);
