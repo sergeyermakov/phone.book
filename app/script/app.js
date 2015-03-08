179dc1e22bd2dde832ff63d06e9f5a7b21aacb84
Xaosaki <xaosaki@gmail.com>
2015-03-08 21:44:21 +0300

    Работает добавление и удаление

--- app/script/app.js
+++ app/script/app.js
@@ -7,16 +7,16 @@ pbApp.value({
 pbApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider
 		.when('/',{
-			templateUrl:'all.html',
+			templateUrl:'list.html',
 			controller:'ListCtrl'
 		})
 		.when('/new',{
-			templateUrl:'contact.html',
-			controller:'newCtrl'
+			templateUrl:'new.html',
+			controller:'NewCtrl'
 		})
 		.when('/user/:userId',{
 			templateUrl:'user.html',
-			controller:'userInfoCtrl'
+			controller:'UserCtrl'
 		})
 		.otherwise({
 			redirectTo: '/'
@@ -26,21 +26,37 @@ pbApp.config(['$routeProvider', function($routeProvider){
 
 //Controllers
 
-pbApp.controller('ListCtrl',['$scope','$location','firebaseFactory', function ($scope, $location,firebaseFactory) {
+pbApp.controller('ListCtrl',['$scope', '$rootScope', '$location','firebaseFactory', function ($scope,$rootScope, $location,firebaseFactory ) {
 	$scope.title = "Контакты";
+	//Список всех контактов
 	$scope.contacts = firebaseFactory.contactsList();
 }]);
 
-pbApp.controller('newCtrl',['$scope','$location', function ($scope, $http, $location) {
+pbApp.controller('NewCtrl',['$scope','firebaseFactory', function ($scope, firebaseFactory) {
 	$scope.title = "Новый контакт";
+	//Добавление контакта
+	$scope.contactsAdd = function(arr){
+		firebaseFactory.contactsAdd(arr);
+	}
+
 }]);
 
-pbApp.controller('userInfoCtrl',['$scope', '$routeParams', 'firebaseFactory', function ($scope, $routeParams,firebaseFactory) {
+pbApp.controller('UserCtrl',['$scope', '$routeParams', 'firebaseFactory', function ($scope, $routeParams,firebaseFactory) {
 	$scope.title = "Изменить контакт";
-
+	//Вывод полной информации о контакте
 	var id =  $routeParams.userId;
 	$scope.id = id;
-	$scope.user = firebaseFactory.contactsEdit(id);
+	$scope.user = firebaseFactory.contactsInfo(id);
+	//Удаление контакта
+	$scope.contactsDel = function(obj){
+		firebaseFactory.contactsDel();
+	}
+	//$scope.contactsEdit = function(){
+	//	firebaseFactory.contactsEdit();
+	//}
+
+
+
 }]);
 
 
@@ -49,16 +65,30 @@ pbApp.controller('userInfoCtrl',['$scope', '$routeParams', 'firebaseFactory', fu
 
 pbApp.factory('firebaseFactory', ['fbURL','$firebaseObject','$firebaseArray', function(fbURL, $firebaseObject, $firebaseArray){
 	var fb = {};
-
+		refBase = new Firebase(fbURL);
+	//Список всех контактов
 	fb.contactsList = function(){
-		var ref = new Firebase(fbURL);
-		return $firebaseArray(ref);
+		return $firebaseArray(refBase);
 	}
-	fb.contactsEdit = function(id){
+	//Добавление контакта
+	fb.contactsAdd = function(arr){
+		return $firebaseArray(refBase).$add(arr);
+	}
+	//Вывод полной информации о контакте
+	fb.contactsInfo = function(id){
 		var url = fbURL+id;
 			ref = new Firebase(url);
 		return $firebaseObject(ref);
 	}
+	//Удаление контакта
+	fb.contactsDel= function(obj){
+		return $firebaseObject(ref).$remove(obj);
+	}
+	////Изменение контакта
+	//fb.contactsEdit= function(){
+	//	return $firebaseObject(ref).$save();
+	//}
+
 
 	return fb;
 }]);
