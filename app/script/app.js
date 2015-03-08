9faebc3518b0cc4c6bf15f4a2da561ec3ae17354
Xaosaki <xaosaki@gmail.com>
2015-03-08 14:00:23 +0300

    Основы взаимодействия с базой, набросок приложения.(Добавления/удаления/редактирования нет)

--- app/script/app.js
+++ app/script/app.js
@@ -1,4 +1,8 @@
-var pbApp = angular.module('pbApp', ['ngRoute']);
+var pbApp = angular.module('pbApp', ['ngRoute', 'firebase']);
+
+pbApp.value({
+	'fbURL': 'https://torrid-fire-9572.firebaseio.com/'
+});
 
 pbApp.config(['$routeProvider', function($routeProvider){
 	$routeProvider
@@ -19,25 +23,42 @@ pbApp.config(['$routeProvider', function($routeProvider){
 		});
 }]);
 
-pbApp.controller('ListCtrl',['$scope','$http','$location', function ($scope, $http, $location) {
-	$scope.title = "Контакты"
-	$http.get('../../contacts.json').success(function(data,status,headers,config){
-		$scope.contacts = data;
-	});
+
+//Controllers
+
+pbApp.controller('ListCtrl',['$scope','$location','firebaseFactory', function ($scope, $location,firebaseFactory) {
+	$scope.title = "Контакты";
+	$scope.contacts = firebaseFactory.contactsList();
 }]);
 
-pbApp.controller('newCtrl',['$scope','$http','$location', function ($scope, $http, $location) {
-	$scope.title = "Новый контакт"
-
-	$http.get('../../contacts.json').success(function(data,status,headers,config){
-		$scope.contacts = data;
-	});
+pbApp.controller('newCtrl',['$scope','$location', function ($scope, $http, $location) {
+	$scope.title = "Новый контакт";
 }]);
 
-pbApp.controller('userInfoCtrl',['$scope','$http','$location','$routeParams', function ($scope, $http, $location, $routeParams) {
+pbApp.controller('userInfoCtrl',['$scope', '$routeParams', 'firebaseFactory', function ($scope, $routeParams,firebaseFactory) {
 	$scope.title = "Изменить контакт";
-	$scope.userId = $routeParams.userId;
-	$http.get('../../contacts.json').success(function(data,status,headers,config){
-		$scope.contacts = data;
-	});
+
+	var id =  $routeParams.userId;
+	$scope.id = id;
+	$scope.user = firebaseFactory.contactsEdit(id);
+}]);
+
+
+//Factory
+
+
+pbApp.factory('firebaseFactory', ['fbURL','$firebaseObject','$firebaseArray', function(fbURL, $firebaseObject, $firebaseArray){
+	var fb = {};
+
+	fb.contactsList = function(){
+		var ref = new Firebase(fbURL);
+		return $firebaseArray(ref);
+	}
+	fb.contactsEdit = function(id){
+		var url = fbURL+id;
+			ref = new Firebase(url);
+		return $firebaseObject(ref);
+	}
+
+	return fb;
 }]);
